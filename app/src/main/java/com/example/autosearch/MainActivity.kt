42026a99fce12ee6939b4535f703179c460577da
UV1L <50074365+UV1L@users.noreply.github.com>
2021-10-04 00:04:26 +0500

    Список

--- app/src/main/java/com/example/autosearch/MainActivity.kt
+++ app/src/main/java/com/example/autosearch/MainActivity.kt
@@ -4,32 +4,53 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.border
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.gestures.Orientation
+import androidx.compose.foundation.gestures.ScrollableState
+import androidx.compose.foundation.gestures.scrollable
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
-import com.example.autosearch.search_page.SearchTextField
+import androidx.compose.ui.unit.dp
+import com.example.autosearch.search_page.*
 import com.example.autosearch.ui.theme.AutoSearchTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            AutoSearchTheme(darkTheme = true) {
+            AutoSearchTheme(darkTheme = false) {
                 // A surface container using the 'background' color from the theme
                 Surface(color = MaterialTheme.colors.background) {
 
                     Column(
-                        horizontalAlignment = Alignment.CenterHorizontally,
-                        modifier = Modifier.fillMaxSize()
+                        modifier = Modifier.fillMaxWidth(),
+                        horizontalAlignment = Alignment.CenterHorizontally
                     ) {
 
                         SearchTextField()
+
+                        Spacer(modifier = Modifier.height(12.dp))
+
+                        LazyColumn(
+                            horizontalAlignment = Alignment.CenterHorizontally,
+                            modifier = Modifier
+                                .fillMaxSize()
+                        ) {
+
+                            item {
+
+                                ContentPhotos()
+                                ContentMessages()
+                                ContentVideos()
+                            }
+                        }
                     }
                 }
             }
--- app/src/main/java/com/example/autosearch/search_page/SearchPage.kt
+++ app/src/main/java/com/example/autosearch/search_page/SearchPage.kt
@@ -2,10 +2,9 @@ package com.example.autosearch.search_page
 
 import android.content.res.Configuration.UI_MODE_NIGHT_YES
 import android.widget.Toast
+import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.interaction.MutableInteractionSource
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.*
@@ -20,11 +19,23 @@ import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.foundation.isSystemInDarkTheme
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.icons.filled.ArrowDropDown
 import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.draw.clipToBounds
 import androidx.compose.ui.focus.onFocusChanged
 import androidx.compose.ui.focus.onFocusEvent
 import androidx.compose.ui.text.toUpperCase
+import androidx.compose.ui.unit.sp
 import com.example.autosearch.ui.theme.AutoSearchTheme
+
+enum class ContentTypes(val type: String) {
+
+    PHOTOS("Фотографии"),
+    MESSAGES("Сообщения"),
+    VIDEOS("Видео")
+}
 
 @Composable
 fun DarkModeButton() {
@@ -85,4 +96,66 @@ fun SearchTextField() {
             disabledIndicatorColor = Color.Transparent
         )
     )
+}
+
+@Composable
+fun DescriptionDivider(description: String, modifier: Modifier? = null) {
+
+    Column(
+        horizontalAlignment = Alignment.Start,
+        modifier = modifier ?: Modifier.padding(top = 12.dp)
+    ) {
+
+        Text(
+            modifier = Modifier.padding(start = 20.dp),
+            text = description,
+            fontSize = 23.sp,
+            color = MaterialTheme.colors.secondaryVariant
+        )
+
+        Spacer(modifier = Modifier.height(5.dp))
+
+        Divider(
+            modifier = Modifier.padding(start = 20.dp, end = 20.dp),
+            color = MaterialTheme.colors.primary,
+            thickness = 1.dp
+        )
+    }
+}
+
+@Composable
+private fun ContentCard(contentType: ContentTypes, content: (() -> Unit)? = null) {
+
+    DescriptionDivider(description = contentType.type, modifier = Modifier.padding(top = 12.dp))
+
+    Card(
+        modifier = Modifier
+            .fillMaxWidth()
+            .defaultMinSize(minHeight = 324.dp)
+            .padding(start = 50.dp, end = 50.dp, top = 12.dp, bottom = 12.dp),
+        backgroundColor = MaterialTheme.colors.secondary
+    ) {
+
+        content?.invoke()
+
+        //какой-нибудь when(contentType) { }
+    }
+}
+
+@Composable
+fun ContentPhotos() {
+
+    ContentCard(contentType = ContentTypes.PHOTOS)
+}
+
+@Composable
+fun ContentMessages() {
+
+    ContentCard(contentType = ContentTypes.MESSAGES)
+}
+
+@Composable
+fun ContentVideos() {
+
+    ContentCard(contentType = ContentTypes.VIDEOS)
 }
